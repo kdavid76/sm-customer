11ef168408bef5c3e738787fad7dbd0a21c38f68
kdavid <mikehammer1902@gmail.com>
2023-03-02 22:29:35 +0000

    SM-CUSTOMER: Fixing multiple MongoDB issue and release pipeline upgrade

--- sm-customer-data-access/src/main/kotlin/com/bkk/sm/mongo/customers/config/CustomersMongoConfig.kt
+++ sm-customer-data-access/src/main/kotlin/com/bkk/sm/mongo/customers/config/CustomersMongoConfig.kt
@@ -1,45 +1,106 @@
 package com.bkk.sm.mongo.customers.config
 
+import com.bkk.sm.common.mongo.MongoConfigurationSupportForSM
+import com.bkk.sm.mongo.customers.repositories.CompanyRepository
+import com.bkk.sm.mongo.customers.repositories.UserRepository
 import com.mongodb.reactivestreams.client.MongoClient
 import com.mongodb.reactivestreams.client.MongoClients
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.mongodb.MongoManagedTypes
 import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory
-import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate
 import org.springframework.data.mongodb.core.SimpleReactiveMongoDatabaseFactory
 import org.springframework.data.mongodb.core.convert.MappingMongoConverter
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions.MongoConverterConfigurationAdapter
+import org.springframework.data.mongodb.core.convert.NoOpDbRefResolver
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories
 import java.util.*
 
 @Configuration
-@EnableReactiveMongoRepositories(basePackages = ["com.bkk.sm.mongo.customers.repositories"])
-class CustomersMongoConfig : AbstractReactiveMongoConfiguration() {
+@EnableReactiveMongoRepositories(
+    basePackageClasses = [UserRepository::class, CompanyRepository::class],
+    reactiveMongoTemplateRef = "customerMongoTemplate",
+)
+class CustomersMongoConfig : MongoConfigurationSupportForSM() {
 
     @Value("\${com.bkk.sm.mongo.customers.database}")
     val database: String = ""
 
     @Value("\${com.bkk.sm.mongo.customers.uri}")
     val uri: String = ""
+    override fun configureConverters(converterConfigurationAdapter: MongoConverterConfigurationAdapter?) {
+        // No extra converters
+    }
 
     override fun getDatabaseName(): String = database
 
-    @Bean(name = ["superUserProperties"])
+    @Bean
+    @Qualifier("superUserProperties")
     @ConfigurationProperties(prefix = "com.bkk.sm.mongo.customers.init.superuser")
     fun superUserProperties(): Properties = Properties()
 
-    @Bean(name = ["customerMongoClient"])
-    override fun reactiveMongoClient(): MongoClient = MongoClients.create(uri)
+    @Bean
+    @Qualifier("customerMongoClient")
+    fun reactiveMongoClient(): MongoClient = MongoClients.create(uri)
 
-    @Bean(name = ["customerMongoDbFactory"])
-    override fun reactiveMongoDbFactory(): ReactiveMongoDatabaseFactory =
-        SimpleReactiveMongoDatabaseFactory(reactiveMongoClient(), databaseName)
-
-    @Bean(name = ["customerMongoTemplate"])
-    override fun reactiveMongoTemplate(
-        databaseFactory: ReactiveMongoDatabaseFactory,
-        mongoConverter: MappingMongoConverter,
+    @Bean("customerMongoTemplate")
+    fun reactiveMongoTemplate(
+        @Qualifier("customerMongoDbFactory") databaseFactory: ReactiveMongoDatabaseFactory,
+        @Qualifier("customerMappingMongoConverter") mongoConverter: MappingMongoConverter,
     ): ReactiveMongoTemplate = ReactiveMongoTemplate(databaseFactory, mongoConverter)
+
+    @Bean
+    @Qualifier("customerMongoDbFactory")
+    fun reactiveMongoDbFactory(): ReactiveMongoDatabaseFactory =
+        SimpleReactiveMongoDatabaseFactory(reactiveMongoClient(), getDatabaseName())
+
+    @Bean
+    @Qualifier("customerMappingMongoConverter")
+    fun mappingMongoConverter(
+        @Qualifier("customerMongoDbFactory") databaseFactory: ReactiveMongoDatabaseFactory?,
+        @Qualifier("customerMongoCustomConversion") customConversions: MongoCustomConversions?,
+        @Qualifier("customerMongoMappingContext") mappingContext: MongoMappingContext?,
+    ): MappingMongoConverter? {
+        val converter = MappingMongoConverter(NoOpDbRefResolver.INSTANCE, mappingContext!!)
+        converter.customConversions = customConversions!!
+        converter.setCodecRegistryProvider(databaseFactory)
+        return converter
+    }
+
+    @Bean
+    @Qualifier("customerMongoMappingContext")
+    fun mongoMappingContext(
+        @Qualifier("customerMongoCustomConversion") customConversions: MongoCustomConversions,
+        @Qualifier("customerMongoManagedTypes") mongoManagedTypes: MongoManagedTypes?,
+    ): MongoMappingContext? {
+        val mappingContext = MongoMappingContext()
+        mappingContext.setManagedTypes(mongoManagedTypes!!)
+        mappingContext.setSimpleTypeHolder(customConversions.simpleTypeHolder)
+        mappingContext.setFieldNamingStrategy(fieldNamingStrategy())
+        mappingContext.isAutoIndexCreation = autoIndexCreation()
+        return mappingContext
+    }
+
+    @Bean
+    @Qualifier("customerMongoCustomConversion")
+    fun customConversions(): MongoCustomConversions {
+        return MongoCustomConversions.create { converterConfigurationAdapter: MongoConverterConfigurationAdapter? ->
+            configureConverters(
+                converterConfigurationAdapter!!,
+            )
+        }
+    }
+
+    @Bean
+    @Qualifier("customerMongoManagedTypes")
+    @Throws(ClassNotFoundException::class)
+    fun mongoManagedTypes(): MongoManagedTypes {
+        return MongoManagedTypes.fromIterable(getInitialEntitySet())
+    }
 }
