7e76861b6c01c04fcdead21b2ce2ba51065980d9
kdavid <mikehammer1902@gmail.com>
2024-05-19 23:18:38 +0100

    SM-CUSTOMER: Helm chart update

--- sm-customer-services/src/main/kotlin/com/bkk/sm/customers/config/TopicConfiguration.kt
+++ sm-customer-services/src/main/kotlin/com/bkk/sm/customers/config/TopicConfiguration.kt
@@ -5,11 +5,10 @@ import org.apache.kafka.clients.admin.AdminClientConfig
 import org.apache.kafka.clients.admin.NewTopic
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.config.TopicBuilder
 import org.springframework.kafka.core.KafkaAdmin
 
-@Configuration
+//@Configuration
 class TopicConfiguration {
 
     @Value("\${spring.kafka.producer.bootstrap-servers}")
